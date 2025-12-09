01c7dfa2f405bb38cdff59cc1cbaba6249b00a85
prasanna-testpress <prasanna@testpress.in>
2025-12-09 16:37:14 +0530

    feat: Implement user registration functionality and add basic account views with associated forms and templates.

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,6 @@ app_name='account'
 urlpatterns=[
     path("login/",auth_views.LoginView.as_view(), name='login'),
     path("logout/",auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
     path("",views.dashboard, name='dashboard'),
 ]
