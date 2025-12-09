10a7f12bbb064360906e4aa036ab253dd90326c3
Rahulprasanna <rahulprasanna012@gmail.com>
2025-12-09 12:57:14 +0530

    Implement login view for user authentication

--- account/views.py
+++ account/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+
+
+def login(request):
+
+    if request.method == "POST":
+
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+
+            cd = form.cleaned_data
+
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+
+            if user is not None:
+                login(request, user)
+                return render(request, "account/dashboard", {"section": "dashboard"})
+            else:
+
+                return render(
+                    request,
+                    "account/login.html",
+                    {"form": form, "error": "Invalid Credentials"},
+                )
+
+    else:
+        form = LoginForm()
+
+    return render(request, "account/login.html", {"form": form})
