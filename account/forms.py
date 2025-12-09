c56f00eedbb0bd0d13f9dbc23d94e426ffd34b6c
Rahulprasanna <rahulprasanna012@gmail.com>
2025-12-09 13:03:26 +0530

    Refactor LoginForm to use forms.Form and add account URL routing

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
-class LoginForm(forms.ModelForm):
+class LoginForm(forms.Form):
     username = forms.CharField(max_length=250)
     password = forms.CharField(widget=forms.PasswordInput)
