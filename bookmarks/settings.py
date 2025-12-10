e5c243ffc6f62ab2fd8c59a1ac63a8c6f95c0069
prasanna-testpress <prasanna@testpress.in>
2025-12-10 11:48:55 +0530

    feat: Implement email authentication backend and configure project settings for account app

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -50,6 +50,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
+
+
 ROOT_URLCONF = 'bookmarks.urls'
 
 TEMPLATES = [
